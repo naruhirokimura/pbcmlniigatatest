1d9fc10b9b604fed3333b6e6ba5a195d55dd1c67
naruhirokimura <112096162+naruhirokimura@users.noreply.github.com>
2022-10-07 11:50:03 +0900

    Update pbcmlniigatatest.py

--- pbcmlniigatatest.py
+++ pbcmlniigatatest.py
@@ -7,7 +7,7 @@ with open('model.pickle', 'rb') as f:
 
 PT = st.sidebar.slider('PT (%)',min_value=0, max_value=130,value=100)
 ALT = st.sidebar.slider(label='ALT (IU/L)', min_value=0, max_value=400,value=40)
-Tbil = st.sidebar.slider(label='T-Bil (mg/dL)', min_value=0, max_value=10,value=1.0, step=0.1)
+Tbil = st.sidebar.slider(label='T-Bil (mg/dL)', min_value=0, max_value=10,value=1.0)
 BUN = st.sidebar.slider(label='BUN (mg/dL)', min_value=0, max_value=130,value=20)
 
 #サンプルデータの読み込み
